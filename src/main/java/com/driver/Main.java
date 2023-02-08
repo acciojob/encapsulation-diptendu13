9c9d07548581892ed2aef2509f1a35597acd1f30
diptendu <mandaldiptendu99@gmail.com>
2023-02-08 20:55:08 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
-  
+  public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      RWOnly rw = new RWOnly();
+      // name has private access in com.driver.RWOnly
+      rw.setName(sc.nextLine());
+      rw.getName();
+  }
 }
